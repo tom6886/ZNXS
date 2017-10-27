739e6f39758836a84de83c398574473a7172a5ad
汤波 <478185912@qq.com>
2017-10-27 17:47:00 +0800

    Initial commit

---
+++ ZNXS.Models/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZNXS.Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- ZNXS/Startup.cs
+++ ZNXS/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ZNXS.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ZNXS
 {
@@ -21,6 +23,9 @@ namespace ZNXS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
@@ -43,7 +48,7 @@ namespace ZNXS
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
+                    template: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
